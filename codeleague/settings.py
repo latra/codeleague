944eebd1220a0e08274801bdb682a17e5fa1ca03
Oriol <oriolalascercos@gmail.com>
2020-03-12 16:54:52 +0100

    Travis done

--- codeleague/settings.py
+++ codeleague/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wr_r#c8=tz!myr!gi6!-0(&09*bkff9fe1jq9!mh!x3wzm6wxl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['codeleague-wp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
