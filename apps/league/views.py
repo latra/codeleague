87bed48cd4168eb0b355166171b24794fc871dc8
Oriol Alàs Cercós <oriolalascercos@gmail.com>
2020-05-03 23:36:46 +0200

    Now it is has created the ListView for competitions

--- apps/league/views.py
+++ apps/league/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
+from apps.league.models import Competition
 
 # Create your views here.
 from django.views import generic
 
-
-class Home(generic.TemplateView):
+print("hi")
+class Home(generic.ListView):
+    model = Competition
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['competitions'] = ['hi', 'hisda']
+        print(context)
         return context
