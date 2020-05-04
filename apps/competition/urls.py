e5fee8c826081d649d1a9b69d73da713f8786a83
= <personadelmoton@gmail.com>
2020-05-04 23:32:28 +0200

    Movida la lista de grupos disponibles a la pantalla de detalles. Cambiada la posicion del boton de crear grupo

--- apps/competition/urls.py
+++ apps/competition/urls.py
@@ -6,7 +6,6 @@ app_name='competition'
 urlpatterns = [
     path('create/', views.CreateCompetitionView.as_view(), name='createcompetition'),
     path('id/<int:pk>/', views.CompetitionDetail.as_view(), name='detail'),
-    path('id/<int:pk>/join-team/', views.JoinTeam.as_view(), name='join'),
     path('id/<int:pk>/create-team/', views.CreateTeam.as_view(), name='create-team'),
     path('categories/', views.Categories.as_view(), name='listofcategories'),
 ]
