cd5139f5d845d0d7130dcd4086651ecaa52778f4
Oriol Alàs Cercós <oriolalascercos@gmail.com>
2020-05-04 15:39:34 +0200

    Bug fixed

--- apps/league/views.py
+++ apps/league/views.py
@@ -11,7 +11,9 @@ class Home(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
-class Info(generic.ListView):
+
+
+class Info(generic.TemplateView):
     template_name = 'info.html'
 
 
