a74d40289dda93bd5caee64720243db0eb7e1034
Marta Albets Mitjaneta <martaalmi@martaalmi.com>
2020-05-10 13:16:35 +0200

    delete competition not finished

--- apps/competition/urls.py
+++ apps/competition/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('create/', views.CreateCompetitionView.as_view(), name='createcompetition'),
     path('id/<int:pk>/', views.CompetitionDetail.as_view(), name='detail'),
     path('id/<int:pk>/edit/', views.CompetitionUpdate.as_view(), name='edit'),
+    path('id/<int:pk>/delete/', views.CompetitionDelete.as_view(), name='delete'),
     path('id/<int:pk>/finish/', views.CompetitionFinish.as_view(), name='finish'),
     path('id/<int:pk>/create-team/', views.CreateTeam.as_view(), name='create-team'),
     path('id/<int:pk>/submit-answer/', views.PublishAnswerCompetition.as_view(), name='submit-answer'),
